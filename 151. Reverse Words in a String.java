1328d0422a07ad3063f4207d21d5d709c6346878
retrouvailles0 <30648195+retrouvailles0@users.noreply.github.com>
2019-11-05 21:06:39 -0600

    update C++ solution with/out a stack

--- 151. Reverse Words in a String.java
+++ 151. Reverse Words in a String.java
@@ -7,6 +7,59 @@ Could the input string contain leading or trailing spaces?
 	Yes. However, your reversed string should not contain leading or trailing spaces.
 How about multiple spaces between two words?
 	Reduce them to a single space in the reversed string.
+	
+此题不难，注意clarification里的细节，要去除头尾空格，单词中间多个空格reverse后只能有一个。
+
+C++
+	
+Time: O(n), Space: O(1)
+void reverseWords(string& s) {
+	int i = 0;
+	int j = 0;
+	int n = s.length();
+	while (i < n) {
+		while (i < n && s[i] == ' ') i++;
+		if (i < n && j > 0) {
+			s[j++] = ' ';
+		}
+		int start = i;
+		while (i < n && s[i] != ' ') {
+			s[j++] = s[i++];
+		}
+		reverse(s.begin() + start, s.begin() + j);
+	}
+	s.resize(j);
+	reverse(s.begin(), s.end());
+}
+
+Use stack
+Time: O(n), Space: O(n)
+string reverseWords(string s) {
+	
+	int i = 0;
+	int n = s.length();
+	stack<string> st;
+	while (i < n) {
+		int start = i;
+		int end = i;
+		while (i < n && s[i] == ' ') {
+			i++;
+		}
+		start = i;
+		while (i < n && s[i] != ' ') {
+			i++;
+		}
+		end = i;
+		if (start == end) break;
+		st.push(s.substr(start, end - start));
+	}
+	string ret = "";
+	while (!st.empty()) {
+		ret += st.top() + " ";
+		st.pop();
+	}
+	return ret.substr(0, ret.size() - 1);
+}
 
 
 Solution：split + 倒序遍历append
