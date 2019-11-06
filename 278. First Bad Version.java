3dc217675d69eeadb333d67d1363f04c1b9311ce
retrouvailles0 <30648195+retrouvailles0@users.noreply.github.com>
2019-11-06 12:43:36 -0600

    Update 278. First Bad Version.java

--- 278. First Bad Version.java
+++ 278. First Bad Version.java
@@ -5,7 +5,29 @@ Unfortunately, the latest version of your product fails the quality check. Since
 Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
 
-    
+Run test case 
+https://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=566963
+
+n = 2: 
+left = 1, right = 2, mid = 1;
+if mid is bad, right = 1, return 1
+if mid is not bad, left = 2, return 2;
+
+int firstBadVersion(int n) {
+    int left = 1;
+    int right = n;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (isBadVersion(mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+
+
 public int firstBadVersion(int n) {
     int left = 1, right = n;
     while (left < right) {
@@ -15,12 +37,4 @@ public int firstBadVersion(int n) {
     }
     return left;
 }
-
-Run test case 
-https://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=566963
-
-n = 2: 
-left = 1, right = 2, mid = 1;
-if mid is bad, right = 1, return 1
-if mid is not bad, left = 2, return 2;
 
